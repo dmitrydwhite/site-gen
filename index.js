44fa4d863c1e0eec693a7d3d60a729dc3a0838a5
Dmitry White <white.dmitry@gmail.com>
2014-06-02 16:21:18 -0700

    It accepts a path or the current directory submitted as a '.' character

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 var readline = require('readline');
+var fs = require('fs');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+var rfOptions = {encoding: 'utf8'};
 
 /**
- * SnowForge is a static-site generator that accepts a directory and
+ * SnowFrog is a static-site generator that accepts a directory and
  * converts it into a directory with the new layout
  * @param {string} path: a path for the file or file system that will be
  *                       the basis for the generated site.
  */
-module.exports.SnowForge = function (directory, indexFile) {
+module.exports.SnowFrog = function (directory, indexFile) {
+  // this.readDirectory(directory, indexFile);
   var template = indexFile;
-  if (!indexFile) {
-    template = directory + "/layout.html";
+  var dr = directory;
+  if (directory === '.') {
+    dr = __dirname;
   }
-  console.log(directory);
+  if (!indexFile) {
+    template = dr + "/layout.html";
+  }
   console.log("Template file is: " + template);
-  var complex = {path: directory, temp: template};
-  return complex;
+  var readObject = {path: dr, template: template};
+  return readObject;
 };
+
+// var readDirectory = function (directory, indexFile) {
+//   var template = indexFile;
+//   var dr = directory;
+//   if (directory === '.') {
+//     dr = __dirname;
+//   }
+//   if (!indexFile) {
+//     template = dr + "/layout.html";
+//   }
+//   console.log("Template file is: " + template);
+//   var readObject = {path: dr, template: template};
+//   return readObject;
+// }.bind(this);
+
+
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,17 @@
-var SnowForge = require('../index');
+var _ = require('../index');
+var chai = require('chai');
+var expect = chai.expect;
 
 describe('site-generator', function () {
   it('locates a directory and an index file', function () {
     var input = "test.html";
-    expect(new SnowForge(input)).to.exist;
+    expect(_.SnowFrog(input)).to.eql({path:'test.html', template:'test.html/layout.html'});
+  });
+
+  it('can open the files in the current directory', function () {
+    var input = ".";
+    expect(_.SnowFrog(input)).to.eql({path:'/Users/dmitrywhite/pcs/js_immersion/site-gen',
+                                      template:'/Users/dmitrywhite/pcs/js_immersion/site-gen/layout.html'});
   });
 
 });
