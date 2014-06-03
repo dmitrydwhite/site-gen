34700a52af160d776845325a190490a8faf6f512
Dmitry White <white.dmitry@gmail.com>
2014-06-03 14:09:19 -0700

    fixed all bugs except the readdir sync issue

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var readline = require('readline');
 var fs = require('fs');
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 var rfOptions = {encoding: 'utf-8'};
 var _ = require('lodash');
+var path = require('path');
 
 /**
  *
@@ -20,38 +17,37 @@ var _ = require('lodash');
  *                           assumes "layout.html"
  */
 var SnowFrog = function (directory, indexFile) {
+  var progress = true;
+  this._findDirectory(directory, indexFile);
   var sourceObj = this._findDirectory(directory, indexFile);
-  var commonLayout;
-  sourceObj.webFiles = this._webList(sourceObj);
-  this._extractTemplate(sourceObj.template);
-  if (commonLayout) {
-  }
-  return sourceObj;
+  this._webList(sourceObj);
+};
+
+SnowFrog.prototype._proceed = function (fn) {
+  fn();
 };
 
 SnowFrog.prototype._findDirectory = function (directory, indexFile) {
   var template = indexFile;
-  var dr = directory;
-  if (directory === '.') {
-    dr = __dirname;
-  }
+  var dr = path.resolve(directory);
   if (!indexFile) {
-    template = dr + "/layout.html";
+    template = path.normalize(dr + "/layout.html");
   }
-  // console.log("Template file is: " + template);
+  console.log("Template file is: " + template);
   var readObject = {path: dr, template: template};
   return readObject;
 };
 
 SnowFrog.prototype._webList = function (sourceDir) {
-  var files = fs.readdirSync(sourceDir.path);
-  var webFiles = [];
-  files.map(function (fileName) {
-    if (fileName.slice(-5) === ".html") {
-      webFiles.push(fileName);
-    }
+  fs.readdir(sourceDir.path, function (err, files) {
+    var webFiles = [];
+    files.map(function (fileName) {
+      if (fileName.slice(-5) === ".html") {
+        webFiles.push(fileName);
+      }
+    });
+  this._proceed(this._extractTemplate(indexFile));
   });
-  return webFiles;
 };
 
 SnowFrog.prototype._findCommon = function (sourceDir) {
@@ -87,8 +83,8 @@ SnowFrog.prototype._findCommon = function (sourceDir) {
 SnowFrog.prototype._extractTemplate = function (layoutFile) {
   fs.readFile(layoutFile, "utf-8", function (err, content) {
     var layoutArray = content.split("{{CONTENT}}");
-    commonLayout = layoutArray;
-    console.log(commonLayout);
+    console.log(layoutArray);
+    return layoutArray;
   });
 };
 
