0e439364e07e18af4c7c64e8fbd4221e03554809
Dmitry White <white.dmitry@gmail.com>
2014-06-04 14:33:01 -0700

    Added a new test with a different input

--- test/tests.js
+++ test/tests.js
@@ -4,7 +4,7 @@ var expect = chai.expect;
 
 describe('site-generator', function () {
 
-  it.only('can locate the current directory', function (done) {
+  it('can locate the current directory', function (done) {
     var input = ".";
     new SnowFrog(input, "", function(err, result) {
       expect(result).
@@ -18,34 +18,18 @@ describe('site-generator', function () {
     });
   });
 
-  it('recognizes all the files in the current directory', function () {
-    var input = ".";
-    expect(new SnowFrog(input)).
-                to.eql({path:'/Users/dmitrywhite/pcs/js_immersion/site-gen',
-                template:'/Users/dmitrywhite/pcs/js_immersion/site-gen/layout.html',
-                templateData: ["This is the desired Header\n\n",
-                  "\n\nThis is the desired footer\n"],
-                webFiles: ['secondtest.html', 'test.html']});
-  });
-
-  it('creates an array of the .html files in the current directory', function () {
-    var input = ".";
-    expect(new SnowFrog(input)).
-                to.eql({path:'/Users/dmitrywhite/pcs/js_immersion/site-gen',
-                template:'/Users/dmitrywhite/pcs/js_immersion/site-gen/layout.html',
-                templateData: ["This is the desired Header\n\n",
-                  "\n\nThis is the desired footer\n"],
-                webFiles: ['secondtest.html', 'test.html']});
-  });
-
-  it('creates a copy of an existing file but with the new template', function () {
-    var input = ".";
-    expect(new SnowFrog(input)).
-                to.eql({path:'/Users/dmitrywhite/pcs/js_immersion/site-gen',
-                template:'/Users/dmitrywhite/pcs/js_immersion/site-gen/layout.html',
-                templateData: ["This is the desired Header\n\n",
-                  "\n\nThis is the desired footer\n"],
-                webFiles: ['secondtest.html', 'test.html']});
+  it('accepts a variable file for the layout file', function (done) {
+    var input = ". index.html";
+    new SnowFrog(input, function(err, result) {
+      expect(result).
+        to.eql({path:'/Users/dmitrywhite/pcs/js_immersion/site-gen',
+        template:'/Users/dmitrywhite/pcs/js_immersion/site-gen/layout.html',
+        templateData: ["This is the desired Header\n\n",
+          "\n\nThis is the desired footer\n"],
+        webContent: 'A bunch of other unique stuff about the second test.\n',
+        webFiles: ['secondtest.html', 'test.html'],});
+      done();
+    });
   });
 
 });
